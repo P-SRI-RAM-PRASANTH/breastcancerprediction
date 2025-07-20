2943102be684c404fbf62bf56320cc1ab4bc621d
PRASANTH PADAVALA <136237006+P-SRI-RAM-PRASANTH@users.noreply.github.com>
2025-07-21 01:52:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ setup(
     name='breastcancerprediction',
     version='0.1',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'numpy==1.21.4',
         'streamlit==1.2.0',
         'protobuf>=3.20.0,<3.21.0',
         'streamlit-option-menu==0.3.2',
         'scikit-learn==1.0.2',
-        'altair==4.2.0',
+        'altair==4.2.0'
     ],
     entry_points={
         'console_scripts': [
-            'run-app = app:main',  # assuming you have a main() in app.py
+            'run-app=app:main',  # assumes you have a `main()` function in app.py
         ],
     },
 )
